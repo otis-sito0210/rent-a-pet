20d1dfa590d4fd6de3885b5b77ab4fb68be44346
otavio gomes <otaviojdggomes@gmail.com>
2023-10-21 17:53:56 -0300

    fix create orders - pet and user

--- db/migrate/20231021201913_create_orders.rb
+++ db/migrate/20231021201913_create_orders.rb
@@ -2,8 +2,8 @@ class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
       t.date :date
-      t.references :pet_id, null: false, foreign_key: true
-      t.references :user_id, null: false, foreign_key: true
+      t.references :pet, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
