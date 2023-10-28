5c53a30d805135c157752fd36e819d7292bae043
otavio gomes <otaviojdggomes@gmail.com>
2023-10-28 16:45:08 -0300

    geocode

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_28_171100) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_28_175902) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_28_171100) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
